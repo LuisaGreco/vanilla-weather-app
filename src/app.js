47b766fdbf9ecc1b4aca435c1b6810b5c5b4d168
LuisaGreco <128385599+LuisaGreco@users.noreply.github.com>
2023-05-28 21:57:46 +0200

    Upgrade current icon from API

--- src/app.js
+++ src/app.js
@@ -9,20 +9,23 @@ function formatDate(timestamp) {
 }
 
 function showTemp(response) {
+    console.log(response.data);
     let tempElement = document.querySelector("#grades");
     let cityElement = document.querySelector("#city");
     let descElement = document.querySelector("#description");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
     tempElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descElement.innerHTML = response.data.condition.description;
     dateElement.innerHTML = formatDate(response.data.time * 1000);
-
+    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+    iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "a5c6f5daeaa8d396de43ot341b40480b";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Paris&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+let city = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemp);
